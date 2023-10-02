33fcb2c9c050aecb46de35c37b1dd155ab971bf2
Franco C Sanchez <francocsanchez@gmail.com>
2023-10-01 21:00:54 -0300

    colors

---
+++ src/global/Colors.js
@@ -0,0 +1,12 @@
+const colors = {
+  citrico: "#FFCC70",
+  perlado: "#FFFADD",
+  celeste: "#8ECDDD",
+  indigo: "#22668D",
+  verdeAgua: "#7FFFD4",
+  marfil: "#FFF8DC",
+  azulOscuro: "#191970",
+  granateOscuro: "#800000",
+};
+
+export { colors };
