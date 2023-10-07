1a3acff4214253f6363f500e2242c3a19606227c
Franco C Sanchez <francocsanchez@gmail.com>
2023-10-07 15:06:42 -0300

    fix title

--- src/navigation/HomeStackNavigator.js
+++ src/navigation/HomeStackNavigator.js
@@ -1,46 +1,48 @@
 import { createStackNavigator } from "@react-navigation/stack";
+import { useSelector } from "react-redux";
 
 import { Home, Drink, Drinks, ListFilter } from "../views";
 
 const Stack = createStackNavigator();
 
 const HomeStackNavigator = () => {
+  const { filterType, filterItem } = useSelector((state) => state.drink);
 
   return (
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{
-            headerTitleAlign: "center",
-            headerTitle: "BUSCAR BEBIDA",
-          }}
-        />
-         <Stack.Screen
-          name="ListFilter"
-          component={ListFilter}
-          options={{
-            headerTitleAlign: "center",
-            headerTitle: "FILTRO POR ....",
-          }}
-        />
-        <Stack.Screen
-          name="Drinks"
-          component={Drinks}
-          options={{
-            headerTitleAlign: "center",
-            headerTitle: "TIPO DE BEBIDA",
-          }}
-        />
-        <Stack.Screen
-          name="Drink"
-          component={Drink}
-          options={{
-            headerTitleAlign: "center",
-            headerTitle: "TIPO DE BEBIDA",
-          }}
-        />
-      </Stack.Navigator>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerTitleAlign: "center",
+          headerTitle: "BUSCAR BEBIDA",
+        }}
+      />
+      <Stack.Screen
+        name="ListFilter"
+        component={ListFilter}
+        options={{
+          headerTitleAlign: "center",
+          headerTitle: filterType.title,
+        }}
+      />
+      <Stack.Screen
+        name="Drinks"
+        component={Drinks}
+        options={{
+          headerTitleAlign: "center",
+          headerTitle: filterItem,
+        }}
+      />
+      <Stack.Screen
+        name="Drink"
+        component={Drink}
+        options={{
+          headerTitleAlign: "center",
+          headerTitle: "BEBIDA",
+        }}
+      />
+    </Stack.Navigator>
   );
 };
 
