bd22b2193b6708528a436c8d7af72db678b6b98e
Franco C Sanchez <francocsanchez@gmail.com>
2023-10-08 20:22:31 -0300

    list ingredients to fav

--- src/views/Shop/Shop.Styles.js
+++ src/views/Shop/Shop.Styles.js
@@ -1,5 +1,30 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    flex: 1,
+  },
+  ingredientView: {
+    marginBottom: 60,
+    marginTop: 20,
+  },
+  ingredientContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+    marginHorizontal: 20,
+  },
+  ingredientImage: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  textIngredient: {
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    fontSize: 15,
+  },
+});
 
 export { styles };
--- src/views/Shop/Shop.jsx
+++ src/views/Shop/Shop.jsx
@@ -1,20 +1,56 @@
-import React from "react";
-import { View, Text, ScrollView } from "react-native";
+import { useState, useEffect } from "react";
+import { View, Text, ScrollView, Image } from "react-native";
+import axios from "axios";
 
 import { styles } from "./Shop.Styles";
 import { useSelector } from "react-redux";
 
 const Shop = () => {
   const { listDrinkFav } = useSelector((state) => state.drink);
+  const [ingre, setIngre] = useState([]);
 
-  console.log(listDrinkFav);
+  useEffect(() => {
+    const fetchData = async () => {
+      for (const id of listDrinkFav) {
+        try {
+          const response = await axios.get(
+            `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+          );
+
+          const ingredientsList = [];
+
+          for (let i = 1; i <= 15; i++) {
+            let ingredient = response.data.drinks[0][`strIngredient${i}`];
+            if (ingredient) {
+              ingredientsList.push(ingredient);
+            }
+          }
+          let iTotals = [...iTotals, ingredientsList];
+          setIngre([...new Set([].concat(...iTotals.filter(Boolean)))]);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
+
+    fetchData();
+  }, [listDrinkFav]);
+
   return (
-    <ScrollView>
-      {listDrinkFav?.map((item, index) => (
-        <View key={index}>
-          <Text>Tragos seleccionados {index + 1}</Text>
-        </View>
-      ))}
+    <ScrollView style={styles.container}>
+      <View style={styles.ingredientView}>
+        {ingre?.map((ingredient, index) => (
+          <View key={index} style={styles.ingredientContainer}>
+            <Image
+              source={{
+                uri: `https://www.thecocktaildb.com/images/ingredients/${ingredient}-Small.png`,
+              }}
+              style={styles.ingredientImage}
+            />
+            <Text style={styles.textIngredient}>{ingredient}</Text>
+          </View>
+        ))}
+      </View>
     </ScrollView>
   );
 };
